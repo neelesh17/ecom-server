7bf9f7fc420043a087c315448c687fa67d72f132
Neelesh Thakur <nt00605@gmail.com>
2020-11-11 19:47:26 +0530

    added mongo db atlas url for connection

--- config/env/development.js
+++ config/env/development.js
@@ -3,9 +3,8 @@ module.exports = {
     datastores: {
       default: {
         adapter: 'sails-mongo',
-        host: 'localhost',
-        port: 27017,
-        database: 'ecom'
+        url: 'mongodb://minor:Locals123@ecom-shard-00-00.2yawp.mongodb.net:27017,ecom-shard-00-01.2yawp.mongodb.net:27017,ecom-shard-00-02.2yawp.mongodb.net:27017/ecom?ssl=true&replicaSet=atlas-9a5ge1-shard-0&authSource=admin&retryWrites=true&w=majority',
+        // srl:true,
       }
     }
   
